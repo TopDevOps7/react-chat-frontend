1943d42a16706b08ee06e0f8319db2c0afd8bb08
LorexABC <maxboxtool@gmail.com>
2023-03-22 21:32:51 -0800

    first

---
+++ src/utils/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+// ----------------------------------------------------------------------
+
+// const URL =
+// process.env.NODE_ENV !== 'production' ? 'http://localhost:4000/api/v1' : 'http://localtalk.mobi:4000/api/v1';
+
+// const URL = 'http://ves.qtp.mybluehost.me:4000/api/v1';
+// const URL = 'https://api.localtalk.mobi/api/v1';
+const URL = 'http://194.163.190.143:58080/api/v1';
+
+const axiosInstance = axios.create({
+  baseURL: URL
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+);
+
+export default axiosInstance;
